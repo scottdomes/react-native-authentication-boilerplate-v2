ed99eca01b01505185b47f7a3b41ad695859ef49
Scott Domes <sdomes@indeed.com>
2019-12-28 14:56:18 -0800

    Creating screens

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>LoginScreen</Text>
+      </View>
+    );
+  }
+}
