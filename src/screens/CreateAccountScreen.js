17e4c09f59fe3c92691b95c086b6d20cbd76ac4b
Scott Domes <sdomes@indeed.com>
2019-12-28 15:45:25 -0800

    mock create account

--- src/screens/CreateAccountScreen.js
+++ src/screens/CreateAccountScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { createAccount } from '../api/mock';
 
 const CreateAccount = ({ navigation }) => {
+  const createUser = () => {
+    createAccount('test@test.ca', 'password')
+      .then((val) => {
+        navigation.navigate('Home');
+      })
+      .catch((err) => console.log('error:', err));
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>CreateAccount</Text>
-      <Button
-        title="Log in"
-        onPress={() => navigation.navigate('Login')}
-      />
+      <Button title="Create user" onPress={createUser} />
+      <Button title="Log in" onPress={() => navigation.navigate('Login')} />
     </View>
   );
 };
