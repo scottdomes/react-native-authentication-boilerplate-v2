544bc5579a42925738e712820a9a6b82b20e9c3c
Scott Domes <sdomes@indeed.com>
2019-12-31 13:14:56 -0800

    Controlling values

--- src/forms/Form.js
+++ src/forms/Form.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, View } from 'react-native';
+
+const getInitialState = (fieldKeys) => {
+  const state = {};
+  fieldKeys.forEach((key) => {
+    state[key] = '';
+  });
+
+  return state;
+};
 
 const Form = ({ fields }) => {
   const fieldKeys = Object.keys(fields);
+  const [values, setValues] = useState(getInitialState(fieldKeys));
+
+  const onChangeValue = (key, value) => {
+    const newState = { ...values, [key]: value };
+    setValues(newState);
+  };
 
   return fieldKeys.map((key) => {
     const field = fields[key];
     return (
       <View key={key}>
         <Text>{field.label}</Text>
-        <TextInput {...field.inputProps} />
+        <TextInput
+          {...field.inputProps}
+          value={values[key]}
+          onChangeText={(text) => onChangeValue(key, text)}
+        />
       </View>
     );
   });
